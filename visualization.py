62872e871f088221b8dbcbf5c3427acaaf60a11b
NuwanthiYapa <35219619+NuwanthiYapa@users.noreply.github.com>
2019-02-17 08:52:24 +0530

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -5,6 +5,8 @@ Created on Sun Feb 17 06:51:08 2019
 @author: Nuwanthi
 """
 
+#This outputs different types of visualization diagrams
+
 import pandas as pd
 # Import the module for plotting
 import matplotlib.pyplot as plt
@@ -18,7 +20,6 @@ plt.style.use('ggplot') # Sets the plotting display theme to ggplot2
 plt.show(df.plot(kind = 'box'))
 
 plt.show(sns.distplot(df.ix[:,5], rug = True, bins = 15))
-
 
 with sns.axes_style("white"):
     plt.show(sns.jointplot(df.ix[:,5], df.ix[:,6], kind = "kde"))
